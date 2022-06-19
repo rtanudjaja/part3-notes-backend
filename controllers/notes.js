2ab97202d2292c9da730046d708e2d4161f095f1
Ricky Tanudjaja CT <ricky.tanudjaja@cartrack.com>
2022-06-19 15:37:17 +0700

    add try and catch

--- controllers/notes.js
+++ controllers/notes.js
@@ -27,8 +27,12 @@ notesRouter.post('/', async (request, response, next) => {
     date: new Date(),
   })
 
-  const savedNote = await note.save()
-  response.status(201).json(savedNote)
+  try {
+    const savedNote = await note.save()
+    response.status(201).json(savedNote)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
